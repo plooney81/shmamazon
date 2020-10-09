e4e0c5fc420bff7a8a40001494cc421d61475dff
Pete Looney <petelooney81@gmail.com>
2020-10-09 16:20:04 -0500

    fixed it up

---
+++ migrations/20201009204624-link-users-to-orders.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const { query } = require("express");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'Orders',
+      'userId',
+      {
+          type: Sequelize.DataTypes.INTEGER,
+          references: {
+            model: 'Users', // name of Target table
+            key: 'id', // key in Target table
+          },
+          onUpdate: 'CASCADE',
+          onDelete: 'SET NULL',
+        
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(
+      'Orders',
+      'userId'
+    )
+  }
+};
