e63c1cb16a2a41c17ca38753d7916be957b553e5
Pete Looney <petelooney81@gmail.com>
2020-10-09 15:45:26 -0500

    updates

---
+++ models/orders.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Orders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Orders.init({
+    tracking: DataTypes.STRING,
+    address: DataTypes.STRING,
+    state: DataTypes.STRING,
+    country: DataTypes.STRING,
+    paid: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Orders',
+  });
+  return Orders;
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const db = require('../models');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/register', function(req, res, next) {
+  res.render('users/register');
 });
 
+router.post('/register', (req, res)=>{
+  const { firstName, lastName, email } = req.body
+  db.User.create({
+    first_name: firstName,
+    last_name: lastName,
+    email: email
+  })
+  .then((user)=>{
+    res.json(user);
+  })
+})
+
+// router.get('/orders', (req, res)=>{
+//   db.Orders.findAll()
+//     .then((orders)=>{
+//       res.render('users/orders', {
+//         locals:{
+//           orders: orders
+//         }
+//       });
+//     })
+// })
 module.exports = router;
